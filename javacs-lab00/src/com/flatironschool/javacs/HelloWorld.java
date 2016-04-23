34c908d1d50c695acd34413406bb8fa259b1a5ba
Joanne Jungwon Jang <joanne.jang@stanford.edu>
2016-04-22 21:05:02 -0700

    java version

--- javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
+++ javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
@@ -2,10 +2,12 @@ package com.flatironschool.javacs;
 
 public class HelloWorld {
     public static Double getVersion() {
-        // I won't work!
+    	String java_version = System.getProperty("java.specification.version");
+        return Double.parseDouble(java_version);
     }
 
     public static void main(String[] args) {
-	    // you can test the output of getVersion() here
+	    Double java_version = getVersion();
+	    System.out.println(java_version);
     }
 }
